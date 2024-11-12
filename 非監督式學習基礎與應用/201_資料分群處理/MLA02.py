dd90b1aeba0b70402508ce9c47c8ae6217125896
Li732375 <lee732375@gmail.com>
2024-11-12 11:04:33 +0800

    Update MLA02.py
    移除部分冗，並追加其他同義作法

--- 非監督式學習基礎與應用/201_資料分群處理/MLA02.py
+++ 非監督式學習基礎與應用/201_資料分群處理/MLA02.py
@@ -13,7 +13,6 @@ X, y = make_blobs(n_samples = 200, centers = 4, cluster_std = 0.5,
 
 # 測試不同分群數
 k = 1
-best_cluster_count = None
 inertia = 91
 
 while inertia > 90: # 直到 inertia 未大於 90 時停止
@@ -26,6 +25,19 @@ while inertia > 90: # 直到 inertia 未大於 90 時停止
     inertia = kmeans.inertia_
     k += 1
 
+# =============================================================================
+# # 測試不同分群數
+# for k in range(1, 10):  # 設定合理的範圍
+#     kmeans = KMeans(n_clusters=k, init='k-means++', n_init=15, 
+#                     max_iter=200, random_state=0)
+#     kmeans.fit(X)
+#     
+#     print(f"inertia = {kmeans.inertia_}, 分群數量為: {k}")
+#     
+#     if kmeans.inertia_ <= 90:
+#         break
+# 
+# =============================================================================
 
 # 實作 K-means 演算法
 kmeans = KMeans(n_clusters = 4, init = 'k-means++', n_init = 15, 
