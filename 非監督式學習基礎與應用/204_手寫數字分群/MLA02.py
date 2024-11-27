9dad35099d65d1b627079bc3abe0fd716974bcd3
Li732375 <lee732375@gmail.com>
2024-11-27 14:34:36 +0800

    Update MLA02.py
    更新為官方公告版本下，適用參考答案

--- 非監督式學習基礎與應用/204_手寫數字分群/MLA02.py
+++ 非監督式學習基礎與應用/204_手寫數字分群/MLA02.py
@@ -7,26 +7,21 @@ seed = 0    # 亂數種子數
 import numpy as np
 from sklearn.datasets import load_digits
 
-# 載入手寫數字資料集
 data = load_digits()
+#print(data)
+# 載入手寫數字資料集
 X_digits = data.data
 y_digits = data.target
-#print(data)
 
 # 特徵標準化(scale/StandardScaler)
+#from sklearn.preprocessing import scale
 from sklearn.preprocessing import StandardScaler
 # TODO
 s = StandardScaler()
-s_X = s.fit_transform(X_digits)
-# =============================================================================
-# from sklearn.preprocessing import scale
-# s_X = scale(X_digits)
-# =============================================================================
-
+X = s.fit_transform(X_digits)
 
 # 取出資料集的數字類別數
 n_digits = 10
-
 
 # 建立兩個 K-Means 模型，除以下參數設定外，其餘為預設值
 # #############################################################################
@@ -35,13 +30,8 @@ n_digits = 10
 # #############################################################################
 from sklearn.cluster import KMeans
 # TODO
-kmean1 = KMeans(init = 'k-means++', n_clusters = n_digits, n_init = 10, 
-                random_state = seed)
-kmean1.fit(s_X)
-
-kmean2 = KMeans(init = 'random', n_clusters = n_digits, n_init = 10, 
-                random_state = seed)
-kmean2.fit(s_X)
+k1 = KMeans(init='k-means++', n_clusters=n_digits, n_init=10, random_state=seed)
+k2 = KMeans(init='random', n_clusters=n_digits, n_init=10, random_state=seed)
 
 # 利用 PCA 結果建立 K-Means 模型，除以下參數設定外，其餘為預設值
 # #############################################################################
@@ -50,12 +40,10 @@ kmean2.fit(s_X)
 # #############################################################################
 from sklearn.decomposition import PCA
 # TODO
-pca = PCA(n_components = n_digits, random_state = seed)
-pca.fit(s_X)
-kmean3 = KMeans(init = pca.components_, n_clusters = n_digits, n_init = 1, 
-                random_state = seed)
-kmean3.fit(s_X)
+pca = PCA(n_components=n_digits, random_state=seed)
+pca.fit(X)
 
+k3 = KMeans(init=pca.components_, n_clusters=n_digits, n_init=1, random_state=seed)
 
 # 分別計算上述三個 K-Means 模型的輪廓係數(Silhouette coefficient)與
 # 分類準確率(accuracy)，除以下參數設定外，其餘為預設值
@@ -64,18 +52,24 @@ kmean3.fit(s_X)
 # #############################################################################
 from sklearn.metrics import silhouette_score
 from sklearn.metrics import accuracy_score
-lst_name = ['K-Mean (k-means++)', 'K-Means (random)', 'K-Means (PCA-based)']
+#lst_name = ['K-Mean (k-means++)', 'K-Means (random)', 'K-Means (PCA-based)']
 # TODO
-sils = [silhouette_score(s_X, kmean1.labels_), 
-        silhouette_score(s_X, kmean2.labels_),
-        silhouette_score(s_X, kmean3.labels_)]
+silhouette_s = []
+accs = []
 
-accs = [accuracy_score(y_digits, kmean1.labels_),
-        accuracy_score(y_digits, kmean2.labels_),
-        accuracy_score(y_digits, kmean3.labels_)]
+for cl in [k1, k2, k3]:
+    print(X.shape)
+    
+    cl.fit(X)
+    
+    sil = silhouette_score(X, cl.labels_)
+    silhouette_s.append(sil)
+    
+    acc = accuracy_score(y_digits, cl.predict(X))
+    accs.append(acc)
+    
+    print('sil =', round(sil, 4), ' acc =', round(acc, 4))
 
-for n, s, a in zip(lst_name, sils, accs):
-    print(n, 'sil =', round(s, 4), 'acc =', round(a, 4))
 
 # 進行 PCA 降維後再做 K-Means，除以下參數設定外，其餘為預設值
 # #############################################################################
@@ -83,15 +77,19 @@ for n, s, a in zip(lst_name, sils, accs):
 # PCA: n_components=2, random_state=seed
 # #############################################################################
 # TODO
+pca2 = PCA(n_components=2, random_state=seed)
+pca2_X = pca2.fit_transform(X)
 
-pca = PCA(n_components = 2, random_state = seed)
-pca.fit(s_X)
-kmean4 = KMeans(init = 'k-means++', n_clusters = n_digits, n_init = 10, 
-                random_state = seed)
-kmean4.fit(s_X)
+k4 = KMeans(init='k-means++', n_clusters=n_digits, n_init=10, random_state=seed)
+k4.fit(pca2_X)
 
-print('kmean4 PCA+KMeans Silhouette =', round(silhouette_score(s_X, kmean4.labels_), 4))
-print('kmean4 Accuracy =', round(accuracy_score(y_digits, kmean4.labels_), 4))
+sil = silhouette_score(pca2_X, k4.labels_)
+silhouette_s.append(sil)
 
-accs.append(accuracy_score(y_digits, kmean4.labels_))
-print('highst Accuracy =', round(max(accs), 4))
+acc = accuracy_score(y_digits, k4.predict(pca2_X))
+accs.append(acc)
+
+print('PCA+KMeans Silhouette= ', round(sil, 4))
+print('Accuracy= ', round(acc, 4))
+
+print('MAX Accuracy= ', round(max(accs), 4))
