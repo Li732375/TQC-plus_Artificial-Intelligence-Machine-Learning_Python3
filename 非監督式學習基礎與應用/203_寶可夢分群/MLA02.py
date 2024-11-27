6ab4571571e2fda4521e28e7f44ce331b3a03b0b
Li732375 <lee732375@gmail.com>
2024-11-27 13:06:55 +0800

    Update MLA02.py
    取代原 MLA03.py

--- 非監督式學習基礎與應用/203_寶可夢分群/MLA02.py
+++ 非監督式學習基礎與應用/203_寶可夢分群/MLA02.py
@@ -2,17 +2,18 @@
 # 載入寶可夢資料
 # TODO
 data = pd.read_json('pokemon.json')
+df = data.copy()
 
 # 取出目標欄位
 # TODO
-columns = ["HP", "Attack", "Defense", "SpecialAtk", "SpecialDef"]
-features = data[columns]
+X = df[df.columns[:-1]]
+print(X)
 
 # 特徵標準化
 from sklearn.preprocessing import StandardScaler
 # TODO
-scaler = StandardScaler()
-scaled_features = scaler.fit_transform(features)
+sta = StandardScaler()
+s_X = sta.fit_transform(X)
 
 # 利用 Hierarchical Clustering 進行分群，除以下參數設定外，其餘為預設值
 # #############################################################################
@@ -20,43 +21,42 @@ scaled_features = scaler.fit_transform(features)
 # #############################################################################
 from sklearn.cluster import AgglomerativeClustering
 # TODO
-cluster_model = AgglomerativeClustering(n_clusters = 4, metric = 'euclidean', 
-                                        linkage = 'ward')
-# =============================================================================
-# cluster_model = AgglomerativeClustering(n_clusters = 4, affinity = 'euclidean', 
-#                                         linkage = 'ward')
-# 在新版本 AgglomerativeClustering 中，affinity 這個參數已經被移除或更名。新的 
-# sklearn 版本中，affinity 已經被替換成 metric
-# =============================================================================
-cluster_model.fit(scaled_features)  # 計算分群結果
+cl = AgglomerativeClustering(n_clusters = 4, affinity = 'euclidean', 
+                             linkage = 'ward')
+cl.fit(s_X)
 
 # 計算每一群的個數
 # TODO
-cluster_counts = pd.Series(cluster_model.labels_).value_counts()
+print(cl.labels_)
+cluster_count = pd.Series(cl.labels_).value_counts()
+print(cluster_count)
 
-min_cluster_size = cluster_counts.min()  # 最小群元素個數
-print(f"最小群元素個數: {min_cluster_size}")
-
-max_cluster_size = cluster_counts.max()  # 最大群元素個數
-print(f"最大群元素個數: {max_cluster_size}")
-
+min_c = min(c for c in cluster_count)
+max_c = max(c for c in cluster_count)
+print('Min =', min_c, 'Max =', max_c)
 
 # 找到 Speed 有遺漏值的兩隻寶可夢，並填入組內平均
 # TODO
-print(data[data['Speed'].isna()]) # 有遺漏值的兩隻寶可夢
+print(df[df['Speed'].isna()])
 
-print('\nFill after')
 
-data['Cluster'] = cluster_model.labels_  # 將分群結果加入資料中
-mean_speed_per_cluster = data.groupby('Cluster')['Speed'].mean() # 先分群，後計算每一群 Speed 的平均值
-#print(f"{round(mean_speed_per_cluster)}") # 分類群集的 Speed 平均值
+df['cluster'] = cl.labels_
+clusters_mean = df.groupby('cluster')['Speed'].mean()
+print(clusters_mean)
+print(type(clusters_mean))
 
-nan_index = data[data['Speed'].isna()].index # 取得索引值
-print(data.loc[nan_index])
+nan_df_index = df[df['Speed'].isna()].index
+print(df.loc[nan_df_index])
 
-data.loc[nan_index, 'Speed'] = data['Cluster'].map(round(mean_speed_per_cluster)) # 填補 Speed 遺漏值
-print(data.loc[nan_index]) # 有遺漏值的兩隻寶可夢 Speed
+print('看起來是\n', df['cluster'].map(round(clusters_mean)))
+print('實際上是\n', df['cluster'].map(round(clusters_mean))[nan_df_index])
+df.iloc[nan_df_index, 5] = df['cluster'].map(round(clusters_mean))
+print(df.iloc[nan_df_index])
 # =============================================================================
-# data.iloc[nan_index, 5] = data['Cluster'].map(round(mean_speed_per_cluster)) # 填補 Speed 遺漏值
-# print(data.iloc[nan_index]) # 有遺漏值的兩隻寶可夢 Speed
+# # or
+# df.loc[nan_df_index, 'Speed'] = df['cluster'].map(round(clusters_mean))
+# print(df.loc[nan_df_index])
 # =============================================================================
+
+print(df[:5])
+
